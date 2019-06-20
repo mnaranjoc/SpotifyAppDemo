ced3cba2b133e2cc701d13c9ddb104d909ac6632
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-06-20 17:52:16 -0500

    Get user playlists

--- SpotifyAppDemo/SpotifyAppDemo/Controllers/HomeController.cs
+++ SpotifyAppDemo/SpotifyAppDemo/Controllers/HomeController.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +15,103 @@ namespace SpotifyAppDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.AuthUri = GetAuthUri();
+        SpotifyToken myToken;
 
-            return View();
+        public class SpotifyToken
+        {
+            public string Access_token { get; set; }
+            public string Token_type { get; set; }
+            public int Expires_in { get; set; }
         }
 
-        private string GetAuthUri()
+        public SpotifyToken GetAccessToken()
         {
-            string clientId = "b49eac7e20ae4dac90a11f528089c830";
-            string redirectUri = "http://localhost:54034/";
-            //string scope = "playlist_modify_private";//Scope scope = Scope.PLAYLIST_MODIFY_PRIVATE;
+            SpotifyToken token = new SpotifyToken();
+            string url5 = "https://accounts.spotify.com/api/token";
 
-            return "https://accounts.spotify.com/en/authorize?client_id=" + clientId +
-                "&response_type=token&redirect_uri=" + redirectUri +
-                //"&state=&scope=" + scope + //scope.GetStringAttribute() +
-                "&show_dialog=true";
+
+            var encode_clientid_clientsecret = Convert.
+                ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}",
+                "b49eac7e20ae4dac90a11f528089c830", //clientid
+                "a0449ecc93524cc5a9028d4fe036ac18")));//clientsecret
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url5);
+
+
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Accept = "application/json";
+            webRequest.Headers.Add("Authorization: Basic " + encode_clientid_clientsecret);
+
+
+            var request = ("grant_type=client_credentials");
+            byte[] req_bytes = Encoding.ASCII.GetBytes(request);
+            webRequest.ContentLength = req_bytes.Length;
+
+
+            Stream strm = webRequest.GetRequestStream();
+            strm.Write(req_bytes, 0, req_bytes.Length);
+            strm.Close();
+
+
+            HttpWebResponse resp = (HttpWebResponse)webRequest.GetResponse();
+            String json = "";
+            using (Stream respStr = resp.GetResponseStream())
+            {
+                using (StreamReader rdr = new StreamReader(respStr, Encoding.UTF8))
+                {
+                    //should get back a string i can then turn to json and parse for accesstoken
+                    json = rdr.ReadToEnd();
+                    rdr.Close();
+                }
+            }
+            token = JsonConvert.DeserializeObject<SpotifyToken>(json);
+            return token;
+        }
+
+        private string GetTrackInfo(string url)
+        {
+            string webResponse = string.Empty;
+            myToken = GetAccessToken();
+            try
+            {
+                // Get token for request
+                HttpClient hc = new HttpClient();
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri(url),
+                    Method = HttpMethod.Get
+                };
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Authorization = new AuthenticationHeaderValue("Authorization", "Bearer " + myToken.Access_token);
+                // TODO: Had a task cancelled here, too many errors. Find a better way to process this maybe.
+                // Same code is in GetTrackFeatures function.
+                var task = hc.SendAsync(request)
+                    .ContinueWith((taskwithmsg) => {
+                        var response = taskwithmsg.Result;
+                        var jsonTask = response.Content.ReadAsStringAsync();
+                        webResponse = jsonTask.Result;
+                    });
+                task.Wait();
+
+
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Track Request Error: " + ex.Status);
+            }
+            catch (TaskCanceledException tex)
+            {
+                Console.WriteLine("Track Request Error: " + tex.Message);
+            }
+            return webResponse;
+        }
+
+        public ActionResult Index()
+        {
+            string response = GetTrackInfo("https://api.spotify.com/v1/users/manaco1990/playlists");
+
+            return View();
         }
 
         public ActionResult About()
